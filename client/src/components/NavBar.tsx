d6c2e618554f84e79e185f43ac1eb7a4fdce7351
Prabhat Mishra <138608570+teleport-1254@users.noreply.github.com>
2024-04-22 16:42:43 +0530

    page title added, favicon added

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -5,10 +5,11 @@ const NavBar = () => {
     return (
         <nav className="absolute top-0 m-2 p-3 border-b-2 border-stone-400">
             <div className="container m-auto px-2 w-[1400px] flex justify-between items-center select-none">
-                <Link to={"/"}>
+                <Link to={"/"} className="flex justify-center items-center gap-1">
                     <span className="text-4xl font-sketchit">
                         guessTheDrawing
                     </span>
+                    <img src="/icon.svg" alt="guessTheDrawing" className="size-[40px]" />
                 </Link>
                 <a href="/">
                     <BiLogoGithub size={40} />
